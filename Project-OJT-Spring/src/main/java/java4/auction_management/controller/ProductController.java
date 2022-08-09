d2bfb6c28621a189cf17ec97cfe2b4f9dfbf58f3
truongdxn <daoxuannhattruong@gmail.com>
2022-08-09 21:11:12 +0700

    dev-TruongDXN commit day 9/8

--- Project-OJT-Spring/src/main/java/java4/auction_management/controller/ProductController.java
+++ Project-OJT-Spring/src/main/java/java4/auction_management/controller/ProductController.java
@@ -1,38 +1,27 @@
 package java4.auction_management.controller;
 
 import com.cloudinary.utils.ObjectUtils;
-import com.google.api.client.json.Json;
-import com.google.gson.JsonObject;
 import java4.auction_management.config.CloudinaryConfig;
 import java4.auction_management.entity.auction.Auction;
 import java4.auction_management.entity.bid.Bid;
 import java4.auction_management.entity.category.Category;
 import java4.auction_management.entity.product.Product;
 import java4.auction_management.entity.user.User;
-import java4.auction_management.service.IAuctionService;
 import java4.auction_management.service.IBidService;
 import java4.auction_management.service.ICategoryService;
 import java4.auction_management.service.impl.ProductService;
 import java4.auction_management.service.impl.UserService;
-import org.cloudinary.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.swing.text.html.parser.Entity;
 import javax.validation.Valid;
-import java.awt.*;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.*;
 import java.util.List;
@@ -75,11 +64,10 @@ public class ProductController {
 
     @PostMapping("/create")
     public String createProduct(@Valid @ModelAttribute Product product, BindingResult bindingResult, RedirectAttributes redirectAttributes,
-                                @RequestParam("file") MultipartFile[] files) throws IOException {
+                                @RequestParam("file") MultipartFile[] files, HttpServletRequest httpRequest) throws IOException {
 
         try {
             LocalDateTime datePost = LocalDateTime.now();
-//            String uname = product.getAuction().getUser().getAccount().getUsername();
             User user = userService.getUserByUsername(httpRequest.getUserPrincipal().getName());
             product.getAuction().setUser(user);
             product.setDatePost(datePost);
@@ -115,25 +103,6 @@ public class ProductController {
         return "products/post";
 
     }
-
-
-//     ajax controller history bid
-//    @RequestMapping(value = "/loadBid/{id}", method = RequestMethod.GET,
-//            produces = MediaType.APPLICATION_JSON_VALUE, consumes = {"application/json"})
-//
-//    public @ResponseBody ResponseEntity<Object> sortListBid(@PathVariable("id") Product product) {
-//        List<Bid> bidList = iBidService.listBidSort(product.getProductId());
-//        List<JSONObject> entities = new ArrayList<>();
-//
-//        for (Bid b : bidList) {
-//            JSONObject jsonObject = new JSONObject();
-//            jsonObject.put("id", b.getProduct().getProductId());
-//            entities.add(jsonObject);
-//        }
-//        System.out.println(product.getProductId());
-//        System.out.println(iBidService.listBidSort(product.getProductId()));
-//        return new ResponseEntity<Object>(entities, HttpStatus.OK);
-//    }
 
 
 
