398ca6291c902918d4e84375e8d1b9f433395d00
Hieunguyen0807 <hieunguyen08072001@gmail.com>
2022-08-14 13:31:41 +0700

    Hieu commit day 14/08/2022

--- Project-OJT-Spring/src/main/java/java4/auction_management/service/impl/CartDetailService.java
+++ Project-OJT-Spring/src/main/java/java4/auction_management/service/impl/CartDetailService.java
@@ -11,18 +11,29 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class CartDetailService implements ICartDetailService{
+public class CartDetailService implements ICartDetailService {
+
     @Autowired
     ICartDetailRepository iCartDetailRepository;
 
     @Override
-    public Optional<CartDetail> findCartDetailByProduct(Product product){
-        return iCartDetailRepository.findCartDetailByProduct(product);
+    public Optional<CartDetail> findCartDetailByProduct(Product product) {
+        return Optional.empty();
+    }
+
+    @Override
+    public CartDetail getCartDetailByCartID(Long id) {
+        return iCartDetailRepository.getCartDetailByCartID(id);
     }
 
     @Override
     public List<CartDetail> getAllCartByUserId(Long id) {
         return iCartDetailRepository.getAllCartByUserId(id);
+    }
+
+    @Override
+    public CartDetail getCartDetailByCartDetailID(Long id) {
+        return iCartDetailRepository.getCartDetailByCartDetailID(id);
     }
 
     @Override
@@ -37,9 +48,8 @@ public class CartDetailService implements ICartDetailService{
 
     @Override
     public CartDetail save(CartDetail entity) {
-        return iCartDetailRepository.save(entity);
+        return null;
     }
-
 
     @Override
     public void deleteById(Long id) {
